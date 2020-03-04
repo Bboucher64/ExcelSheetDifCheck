c241dfd364566e0b02886d51e197ce8c75ce18c5
Brad <BBoucher@Healthrise.com>
2020-03-03 22:14:29 -0500

    Add project files.

---
+++ ExcelSheetDifCheck/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ExcelSheetDifCheck
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void SelectFileBtn_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            if (ofd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                TxtCurrentstring.Text = ofd.FileName;
+            }
+        }
+    }
+}
